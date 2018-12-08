de2ce9f4dc626adad0e469de645107291ee56a38
hongtuanquyen <hongtuanquyen@gmail.com>
2018-12-08 10:55:01 +0700

    Update

--- js/bai4.js
+++ js/bai4.js
@@ -1,80 +1,121 @@
 $(document).ready(function() {
- var product = 0;
-    $(".js-menu").click( function() {
+    var product = 0;
+    var timer;
+    $(".js-menu").click(function() {
         $(".js-menu__list").show();
         timeOut(".js-menu");
 		});
-    
-    $(".js-menu__item").click( function(event) {
+
+    $(".js-menu__item").click(function(event) {
         var index = $(".js-menu__item").index($(this));
         $(".js-menu").attr("src", event.target.src);
         $(".js-menu__list").hide();
+        resetTimer();
         moveItem(index);
         timeOut(".js-menu__item");
 		});
-    
-		$(".js-arrow__left").click( function(event) {
+
+    $(".js-arrow__left").click(function(event) {
+        resetTimer();
         moveLeft();
-        $(".js-menu").attr("src", $(".js-menu__item").eq(product).attr("src"));
         timeOut(".js-arrow__left");
-		});
-    
-		$(".js-arrow__right").click( function() {
-        moveRight();
         $(".js-menu").attr("src", $(".js-menu__item").eq(product).attr("src"));
-        timeOut(".js-arrow__right");
 		});
     
-		/*
-			Move to the left side
-			@params: N/A
-			@return: N/A
-		*/    
-		function moveLeft() {
+    $(".js-arrow__right").click(function() {
+        resetTimer();
+        moveRight();
+        timeOut(".js-arrow__right");
+        $(".js-menu").attr("src", $(".js-menu__item").eq(product).attr("src"));      
+		});
+
+    $(".js-list__products").on("swipeleft", function(){
+        resetTimer();
+        moveRight();
+        timeOut(this);
+        $(".js-menu").attr("src", $(".js-menu__item").eq(product).attr("src"));      
+    });
+
+    $(".js-list__products").on("swiperight", function(){
+        resetTimer();
+        moveLeft();
+        timeOut(this);
+        $(".js-menu").attr("src", $(".js-menu__item").eq(product).attr("src"));      
+    }); 
+    
+    /*
+      Move to the left side
+      @params: N/A
+      @return: N/A
+    */    
+    function moveLeft() {
         product--;
         if(product < 0) product = 4;
         moveItem(product);
-		}
+    }
 
-		/*
-			Move to the right side
-			@params: N/A
-			@return: N/A
-		*/    
-		function moveRight() {
+    /*
+      Move to the right side
+      @params: N/A
+      @return: N/A
+    */    
+    function moveRight() {
         product++;
         if(product > 4) product = 0;
         moveItem(product);
-		}
+    }
     
-		/*
-			Move to the next item
-			@params: N/A
-			@return: N/A
-		*/
+    /*
+      Move to the next item
+      @params: N/A
+      @return: N/A
+    */
     function moveItem(offset) {
         var position = 360 * offset;
         $(".js-list__products").animate({left: -position}, 500);        
     }
-    
-		/*
-			Set time out
-			@params: N/A
-			@return: N/A
-		*/
-		function timeOut(item) {
-				$(item).css({pointerEvents: "none"});
-				setTimeout( function(){
-						$(item).css({pointerEvents: "auto"})
-				}, 500)
-		}
-    $(".js-list__products").on("swipeleft",function(){
+
+    /*
+      Set time out
+      @params: item
+      @return: N/A
+    */
+    function timeOut(item) {
+        $(item).css({pointerEvents: "none"});
+        setTimeout(function(){
+        $(item).css({pointerEvents: "auto"})
+        }, 500)
+    }
+
+    /*
+      Reset Timer
+      @params: N/A
+      @return: N/A
+    */       
+    function resetTimer() {
+        clearInterval(timer);
+        timer = setInterval(autoSlide, 3000);      
+    }
+
+    /*
+      Automate slide item
+      @params: N/A
+      @return: N/A
+    */    
+    function autoSlide() {
         moveRight();
-        timeOut(this);
-    }); 
-    $(".js-list__products").on("swiperight",function(){
-        moveLeft();
-        timeOut(this);
-    }); 
+        $(".js-menu").attr("src", $(".js-menu__item").eq(product).attr("src"));
+    }
+
+    /*
+      Initialize 
+      @params: N/A
+      @return: N/A
+    */
+    function init() {
+        resetTimer();
+    }
+    
+    init();
 });
 
